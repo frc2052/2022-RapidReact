c9fca407e093f610882522f303f0a668a674769b
Tomas Sepulveda <tomassepulvedagordon@gmail.com>
2022-01-22 11:55:49 -0600

    Imported TalonSRX, Worked on Hood OneWheelFly

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,13 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final class Motors {
+
+        public static final int  kHoodMotorID = 1;
+        
+
+    }
+
+}
--- src/main/java/frc/robot/subsystems/OneWheelFly.java
+++ src/main/java/frc/robot/subsystems/OneWheelFly.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
-public class OneWheelFly {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class OneWheelFly extends SubsystemBase {
     // One wheel fly wheel code will be below!
+
 
 
 }
