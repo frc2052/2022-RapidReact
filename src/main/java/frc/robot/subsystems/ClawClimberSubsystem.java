e36f0dff95bd2ac5770bc484db6e75cbbe69b838
Loyce <loycetj@gmail.com>
2022-01-29 15:55:56 -0600

    Code for subsystem added

---
+++ src/main/java/frc/robot/subsystems/ClawClimberSubsystem.java
@@ -0,0 +1,12 @@
+public class ClawClimberSubsystem {
+ 
+     // TODO - make motor that will spin the armm to a cretain point 
+public void rotateArm(Boolean reverse);
+public Boolean getClawALimitSwitch();
+public Boolean getClawBLimitSwitch();
+      // TODO - make 2 speatrte pistons that close and open the arm
+      public void openClawA();
+      public void openClawB();
+      public void closeClawA();
+      public void closeClawB();
+}
