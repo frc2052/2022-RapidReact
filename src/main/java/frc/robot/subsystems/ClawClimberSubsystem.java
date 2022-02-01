55f9a7d29b194b11569134d95e738978e3dbc4fd
Loyce <loycetj@gmail.com>
2022-01-31 20:24:12 -0600

    Added some code for the climber subsystem

--- src/main/java/frc/robot/subsystems/ClawClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawClimberSubsystem.java
@@ -1,12 +1,41 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import frc.robot.Constants;
+
 public class ClawClimberSubsystem {
- 
-     // TODO - make motor that will spin the armm to a cretain point 
-public void rotateArm(Boolean reverse);
-public Boolean getClawALimitSwitch();
-public Boolean getClawBLimitSwitch();
-      // TODO - make 2 speatrte pistons that close and open the arm
-      public void openClawA();
-      public void openClawB();
-      public void closeClawA();
-      public void closeClawB();
+      private Solenoid closeASolenoid;
+      private Solenoid openASolenoid;
+      private Solenoid closeBSolenoid;
+      private Solenoid openBSolenoid;
+      private Talon armMotor;
+
+      // TODO - make motor that will spin the armm to a cretain point talonsrx
+      public void rotateArm(){
+            armMotor.set(ControlMode.PercentOutput, Constants..kIntakeSpeed);
+      }
+      public boolean getClawALimitSwitch(){
+            return false;
+      }
+      public boolean getClawBLimitSwitch(){
+            return false;
+      }
+
+      public void openClawA(){
+            openASolenoid.set(true);
+            closeASolenoid.set(false);
+      }
+      public void openClawB(){
+            openBSolenoid.set(true);
+            closeBSolenoid.set(false);
+      }
+      public void closeClawA(){
+            openASolenoid.set(false);
+            closeASolenoid.set(true);
+      }
+      public void closeClawB(){
+            openBSolenoid.set(false);
+            closeBSolenoid.set(true);
+      }
 }
