dc6fa54c09008f8fb5a2d8c57fdf8aca3718ca07
BQLWPF2\Student <info@team2052.com>
2022-02-03 21:03:16 -0600

    for making a proper rotated auto here

--- src/main/java/frc/robot/auto/Simple3BallAutoFrontIntake.java
+++ src/main/java/frc/robot/auto/Simple3BallAutoFrontIntake.java
@@ -6,6 +6,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.commands.TurnInPlaceCommand;
 import frc.robot.commands.VisionTurnInPlaceCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
@@ -16,12 +17,14 @@ public class Simple3BallAutoFrontIntake extends AutoBase {
         super(drivetrain);
         vision.setLED(LEDMode.OFF);
         
+        VisionTurnInPlaceCommand autoAim1 = new VisionTurnInPlaceCommand(drivetrain, vision);
+
         Pose2d startPos = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
         Pose2d ball1Pos = new Pose2d(Units.inchesToMeters(50), 0, Rotation2d.fromDegrees(0));
 
         Pose2d startPos2 = new Pose2d(Units.inchesToMeters(50), 0, Rotation2d.fromDegrees(180));
         Pose2d ball1rotate = new Pose2d(Units.inchesToMeters(35), Units.inchesToMeters(-30), Rotation2d.fromDegrees(180));
-        Supplier<Rotation2d> rotate180 = () -> { return Rotation2d.fromDegrees(180); };
+        Supplier<Rotation2d> rotate180 = () -> { return Rotation2d.fromDegrees(160); };
         
         // TODO: Supplier<Rotation2d> visionRotation = () -> { return ; };
 
@@ -38,6 +41,9 @@ public class Simple3BallAutoFrontIntake extends AutoBase {
         SwerveControllerCommand driveToBall2 = super.ceateSwerveTrajectoryCommand(super.m_slowTrajectoryConfig, start2, ball2Pos, aimNeg130DegreesRight);
         SwerveControllerCommand driveToShoot = super.ceateSwerveTrajectoryCommand(super.m_slowTrajectoryConfig, super.getLastEndingPosCreated(), shootPos, aimNeg45DegreesRight);
         VisionTurnInPlaceCommand autoAim2 = new VisionTurnInPlaceCommand(drivetrain, vision);
+
+        this.addCommands(autoAim1);
+        this.addCommands(new TurnInPlaceCommand(drivetrain, Rotation2d.fromDegrees(160)));
 
         //TODO: extend intake and turn on intake motors
         this.addCommands(driveToBall1); // Drives to the closest ball to the robot
