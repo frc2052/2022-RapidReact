2f3f04aa08c12fbd40840e5fe125c2378803fa71
Knightkrawler <info@team2052.com>
2022-02-05 13:38:54 -0600

    2-5-2022

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,12 +2,13 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.motorcontrol.Victor;
-//what is this for? 
 
-public class Intake {
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+    public class Intake extends SubsystemBase {
     private Solenoid outSolenoid;
     private Solenoid inSolenoid;
     private VictorSPX intakeMotor;
