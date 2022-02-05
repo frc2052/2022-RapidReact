03c4ead6ed30a88ce35d6c6cb65aec13aa7327d0
Tomas Sepulveda <tomassepulvedagordon@gmail.com>
2022-02-05 14:52:42 -0600

    Fixed an import error

--- src/main/java/frc/robot/subsystems/TwoWheelFlySubsystem.java
+++ src/main/java/frc/robot/subsystems/TwoWheelFlySubsystem.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Constants.MotorIDs; 
 
 public class TwoWheelFlySubsystem extends SubsystemBase {
