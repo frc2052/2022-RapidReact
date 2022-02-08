724173dcf114107d712a3274ea2dcc4e5bdc855b
BEDIVERE\Bedivere <info@team2052.com>
2022-02-07 20:35:19 -0600

    Made the constants an import

--- src/main/java/frc/robot/commands/PrepareToLaunchCargoCommand.java
+++ src/main/java/frc/robot/commands/PrepareToLaunchCargoCommand.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
+import frc.robot.Constants;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.TwoWheelFlySubsystem;
 
