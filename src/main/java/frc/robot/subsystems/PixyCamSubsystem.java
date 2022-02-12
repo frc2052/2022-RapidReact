13abb9e4cfa23f2e2d97fc2b0574808c0c14391f
Charlie <info@team2052.com>
2022-02-11 19:15:43 -0600

    Now we got rid of the old code

--- src/main/java/frc/robot/subsystems/PixyCamSubsystem.java
+++ src/main/java/frc/robot/subsystems/PixyCamSubsystem.java
@@ -89,83 +89,9 @@ public class PixyCamSubsystem extends SubsystemBase {
 		public int getSigmap() {
 			return sigmap;
 		}
+        
+
+
 	}
 }
 
-// public Block getBlueBiggestBlock() {
-// 	// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
-// 	// does not wait for new data if none is available,
-// 	// and limits the number of returned blocks to 25, for a slight increase in efficiency
-// 	int blockCount = m_pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
-// 	System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
-// 	if (blockCount <= 0) {
-// 		return null; // If blocks were not found, stop processing
-// 	}
-// 	ArrayList<Block> blocks = m_pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
-// 	Block largestBlock = null;
-// 	for (Block block : blocks) { // Loops through all blocks and finds the widest one
-// 		SmartDashboard.putNumber("Pixyblock.signature",  block.getSignature());
-// 		SmartDashboard.putNumber("Pixyblock.X-value",  block.getX());
-// 		SmartDashboard.putNumber("Pixyblock.Y-value",  block.getY());
-// 		SmartDashboard.putNumber("Pixyblock.Witdth",  block.getWidth());
-// 		SmartDashboard.putNumber("Pixyblock.Height",  block.getHeight());
-// 		SmartDashboard.putNumber("Pixyblock.age",  block.getAge());
-
-// 		if (largestBlock == null) {
-// 			largestBlock = block;
-// 		} else if (block.getWidth() > largestBlock.getWidth()) {
-// 			largestBlock = block;
-// 		}
-// 	}
-// 	return largestBlock;
-// }
-
-// public Block getRedBiggestBlock() {
-// 	// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
-// 	// does not wait for new data if none is available,
-// 	// and limits the number of returned blocks to 25, for a slight increase in efficiency
-// 	int blockCount = m_pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG2, 25);
-// 	System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
-// 	if (blockCount <= 0) {
-// 		return null; // If blocks were not found, stop processing
-// 	}
-// 	ArrayList<Block> blocks = m_pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
-// 	Block largestBlock = null;
-// 	for (Block block : blocks) { // Loops through all blocks and finds the widest one
-// 		if (largestBlock == null) {
-// 			largestBlock = block;
-// 		} else if (block.getWidth() > largestBlock.getWidth()) {
-// 			largestBlock = block;
-// 		}
-// 	}
-// 	return largestBlock;
-// }
-
-// public double angleToBlueBall(){
-// 	Block biggestBlock = getBlueBiggestBlock();
-// 	if (biggestBlock == null) {
-// 		return 0;
-// 	} else {
-// 		//return Math.abs(((biggestBlock.getX() - 158) / (316 / 60)));
-// 		return biggestBlock.getX()-150*60;
-// 	}
-// }
-
-// public boolean hasBlueBall() {
-// 	return getBlueBiggestBlock() != null;
-// }
-
-// public double angleToRedBall(){
-// 	Block biggestBlock = getRedBiggestBlock();
-// 	if (biggestBlock == null) {
-// 		return 0;
-// 	} else {
-// 		//return Math.abs(((- 158)/(316/60)));
-// 		return (biggestBlock.getX() - PixyCamConstants.IMAGE_WIDTH_PIXELS / 2) * 
-// 			(PixyCamConstants.FOV / PixyCamConstants.IMAGE_WIDTH_PIXELS);
-// 	}
-// }
-
-// public boolean hasRedBall() {
-// 	return getRedBiggestBlock() != null;
-// }
