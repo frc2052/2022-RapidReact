9ff4603969f6fd35fea3150d7cd89c724f2f1d69
BQLWPF2\Student <info@team2052.com>
2022-02-19 15:12:09 -0600

    Lexie made auto changes , alex too

--- src/main/java/frc/robot/auto/RightFiveBallAuto.java
+++ src/main/java/frc/robot/auto/RightFiveBallAuto.java
@@ -1,24 +1,21 @@
 package frc.robot.auto;
 
-import java.util.function.Supplier;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.LEDSubsystem.LEDStatusMode;
-import frc.robot.commands.*;
 import frc.robot.commands.drive.TurnInPlaceCommand;
 import frc.robot.commands.drive.VisionTurnInPlaceCommand;
 import frc.robot.commands.intake.IntakeArmInCommand;
 import frc.robot.commands.intake.IntakeArmOutCommand;
-import frc.robot.commands.shooter.PrepareToLaunchCargoCommand;
+import frc.robot.commands.shooter.ShootCommand;
+import frc.robot.commands.shooter.ShootCommand.ShootMode;
 import frc.robot.subsystems.VisionSubsystem.LEDMode;
-
 public class RightFiveBallAuto extends AutoBase {
-
     /**
      * Position A Start (Far Right Parallel with Outer Tarmac Line) Facing Towards the Hub.
      * 1. Immediatley shoot preloaded cargo, turn around, and intake closest alliance cargo.
@@ -39,40 +36,37 @@ public class RightFiveBallAuto extends AutoBase {
         Pose2d terminalBallMidPointPos = new Pose2d(0, Units.inchesToMeters(210), Rotation2d.fromDegrees(120));
         Pose2d terminalBallPos = new Pose2d(Units.inchesToMeters(-30), Units.inchesToMeters(210), Rotation2d.fromDegrees(-110));
 
-        AutoTrajectoryConfig continousSpeedDriveEnd2mpsTrajectoryConfig = super.createTrajectoryConfig(4, 3, 1, 1, 10, 0, 2);
-        AutoTrajectoryConfig continousSpeedDriveStart2mpsTrajectoryConfig = super.createTrajectoryConfig(4, 3, 1, 1, 10, 2, 0);
-        AutoTrajectoryConfig continousSpeedDriveStartEnd2mpsTrajectoryConfig = super.createTrajectoryConfig(4, 3, 1, 1, 10, 2, 2);
+        AutoTrajectoryConfig realignTrajectoryConfig = super.createTrajectoryConfig(1, 0.5, 1, 3, 1);
 
         VisionTurnInPlaceCommand aimAtHub = new VisionTurnInPlaceCommand(drivetrain, vision);
         TurnInPlaceCommand turnAround = new TurnInPlaceCommand(drivetrain, Rotation2d.fromDegrees(170).minus(drivetrain.getPose().getRotation()));
         SwerveControllerCommand driveToBall1 = super.createSwerveTrajectoryCommand(super.slowTrajectoryConfig, startPos, ball1Pos);
         SwerveControllerCommand driveToBall2 = super.createSwerveTrajectoryCommand(super.slowTrajectoryConfig, super.getLastEndingPosCreated(50), ball2Pos, super.createRotationAngle(50));
         SwerveControllerCommand driveToShoot = super.createSwerveTrajectoryCommand(super.slowTrajectoryConfig, super.getLastEndingPosCreated(), shootPos, super.createHubTrackingSupplier(-45));
-        SwerveControllerCommand driveToTerminalMidPoint = super.createSwerveTrajectoryCommand(continousSpeedDriveEnd2mpsTrajectoryConfig, super.getLastEndingPosCreated(), terminalBallMidPointPos, super.createRotationAngle(120));
-        SwerveControllerCommand driveToTerminalBalls = super.createSwerveTrajectoryCommand(continousSpeedDriveStart2mpsTrajectoryConfig, super.getLastEndingPosCreated(-110), terminalBallPos, super.createRotationAngle(110));
+        SwerveControllerCommand driveToTerminalMidPoint = super.createSwerveTrajectoryCommand(realignTrajectoryConfig.withEndVelocity(1), super.getLastEndingPosCreated(), terminalBallMidPointPos, super.createRotationAngle(120));
+        SwerveControllerCommand driveToTerminalBalls = super.createSwerveTrajectoryCommand(realignTrajectoryConfig.withStartVelocity(1), super.getLastEndingPosCreated(-110), terminalBallPos, super.createRotationAngle(110));
         SwerveControllerCommand driveBackToShoot = super.createSwerveTrajectoryCommand(super.speedDriveTrajectoryConfig, super.getLastEndingPosCreated(66), shootPos, createHubTrackingSupplier(-45));
-        
         IntakeArmInCommand intakeArmIn = new IntakeArmInCommand(intake, indexer, grassHopper);
         IntakeArmOutCommand intakeArmOut = new IntakeArmOutCommand(intake, indexer, grassHopper);
 
-        PrepareToLaunchCargoCommand launchCargoCommand = new PrepareToLaunchCargoCommand(shooter, indexer, grassHopper, vision); // Adjust when ready to shoot either 1 or 2 cargo individually
+        ShootCommand shoot1CargoCommand = new ShootCommand(ShootMode.SHOOT_SINGLE, shooter, indexer, vision); // Adjust when ready to shoot either 1 or 2 cargo individually
+        ShootCommand shoot2CargoCommand = new ShootCommand(ShootMode.SHOOT_ALL, shooter, indexer, vision);
 
-        // TODO add features for advanced timing control when the shooter or intake should be run during driving commands
-        //ParallelCommandGroup aimAndShootPreloaded = new ParallelCommandGroup(aimAtHub, launchCargoCommand);
-/*      ParallelCommandGroup intakeBall1 = new ParallelCommandGroup(driveToBall1, intakeArmOut);
-        ParallelCommandGroup shoot2Balls = new ParallelCommandGroup(driveToShoot, launchCargoCommand, intakeArmIn);
-        ParallelCommandGroup intakeTerminalBall = new ParallelCommandGroup(driveToTerminalBalls, intakeArmOut);
-        ParallelCommandGroup driveBackAndShoot2 = new ParallelCommandGroup(driveBackToShoot, intakeArmIn, launchCargoCommand); */
+        ParallelDeadlineGroup intakeBall1 = new ParallelDeadlineGroup(driveToBall1, intakeArmOut);
+        ParallelDeadlineGroup shoot2Balls = new ParallelDeadlineGroup(driveToShoot, shoot2CargoCommand, intakeArmIn);
+        ParallelDeadlineGroup intakeTerminalBall = new ParallelDeadlineGroup(driveToTerminalBalls, intakeArmOut);
+        ParallelCommandGroup driveBackAndShoot2 = new ParallelCommandGroup(driveBackToShoot, intakeArmIn);
 
         this.addCommands(aimAtHub);
+        this.addCommands(shoot1CargoCommand.withTimeout(3));
         this.addCommands(turnAround);
-        this.addCommands(driveToBall1); // Drives to the closest ball to the robot
+        this.addCommands(intakeBall1); // Drives to the closest ball to the robot
         this.addCommands(driveToBall2); // Drives and rotates to the second ball near the Tarmac
-        this.addCommands(driveToShoot); // Drives and rotates to position to shoot ball into upper hub\
+        this.addCommands(shoot2Balls); // Drives and rotates to position to shoot ball into upper hub\
         this.addCommands(driveToTerminalMidPoint);
-        this.addCommands(driveToTerminalBalls);//this is where the parallel intake terminal ball command will go.
-        this.addCommands(driveBackToShoot);
-
+        this.addCommands(intakeTerminalBall);
+        this.addCommands(driveBackAndShoot2);
+        this.addCommands(shoot2CargoCommand.withTimeout(3));
         this.andThen(() -> LEDSubsystem.getInstance().setLEDStatusMode(LEDStatusMode.AUTONOMOUS_FINISHED));
         this.andThen(() -> drivetrain.stop(), drivetrain);
     }
---
+++ src/main/java/frc/robot/commands/shooter/NonVisionShootCommand.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+import frc.robot.util.vision.ShooterDistanceConfig;
+import frc.robot.util.vision.VisionCalculator;
+
+public class NonVisionShootCommand extends CommandBase {
+  private final ShooterSubsystem shooter;
+  private final IndexerSubsystem indexer;
+  // Determines whether all the balls should be shot and both feeder and preloader should be run (true)
+  // or if only one ball should be shot and only the feeder should be run (false).
+  // This is useful if you pick up the wrong color ball.
+  private final NonVisionShootMode nonVisionShootMode;
+
+  private double topWheelVelocity;
+  private double bottomWheelVelocity;
+
+  public NonVisionShootCommand(NonVisionShootMode shootMode, ShooterSubsystem shooter, IndexerSubsystem indexer, double topWheelVelocity, double bottomWheelVelocity) {
+    this.shooter = shooter;
+    this.indexer = indexer;
+    this.nonVisionShootMode = shootMode;
+    this.topWheelVelocity = topWheelVelocity;
+    this.bottomWheelVelocity = bottomWheelVelocity;
+
+    addRequirements(shooter, indexer);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    shooter.runAtShootSpeed(topWheelVelocity, bottomWheelVelocity);
+
+    if (shooter.isAtSpeed()) {
+      indexer.runFeeder();
+      if (nonVisionShootMode == NonVisionShootMode.SHOOT_ALL) {
+        indexer.runPreload();
+      }
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    indexer.stopFeeder();
+    indexer.stopPreload();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  public static enum NonVisionShootMode {
+    SHOOT_SINGLE,
+    SHOOT_ALL
+  }
+}
